5278a3bc439df9c877fcee33563c62457a8faf07
Tan Yanbo <tanyanbo95@hotmail.com>
2021-07-24 19:41:06 +0800

    add cover image

--- lib/screens/AuthenticationPage.dart
+++ lib/screens/AuthenticationPage.dart
@@ -21,62 +21,92 @@ class _AuthenticationPageState extends State<AuthenticationPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('星愿'),
-      ),
-      body: Column(
-        children: [
-          Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 50),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: '手机号',
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/cover.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Text('手机号:'),
+                          ),
+                          prefixStyle: TextStyle(color: Colors.black),
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 2,
+                            horizontal: 10,
+                          ),
+                          focusedBorder: OutlineInputBorder(),
+                        ),
+                        validator: (val) {
+                          if (val == null) {
+                            return null;
+                          }
+                          return val.length == 11 ? null : '请输入11位手机号';
+                        },
+                        keyboardType: TextInputType.number,
+                        autovalidateMode: AutovalidateMode.onUserInteraction,
+                        onSaved: (value) {
+                          print(value);
+                        },
                       ),
-                      validator: (val) {
-                        if (val == null) {
-                          return null;
-                        }
-                        return val.length == 11 ? null : '请输入11位手机号';
-                      },
-                      keyboardType: TextInputType.number,
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      onSaved: (value) {
-                        print(value);
-                      },
-                    ),
-                    SizedBox(height: 20),
-                    TextFormField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: '验证码',
+                      SizedBox(height: 20),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          prefixIcon: Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Text('验证码:'),
+                          ),
+                          prefixStyle: TextStyle(color: Colors.black),
+                          border: OutlineInputBorder(),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 2,
+                            horizontal: 10,
+                          ),
+                          focusedBorder: OutlineInputBorder(),
+                        ),
+                        keyboardType: TextInputType.number,
+                        onSaved: (value) {
+                          print(value);
+                        },
                       ),
-                      keyboardType: TextInputType.number,
-                      onSaved: (value) {
-                        print(value);
-                      },
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    ElevatedButton(
-                      child: Text('登录'),
-                      onPressed: () => submitForm(context),
-                      style:
-                          ElevatedButton.styleFrom(minimumSize: Size(200, 40)),
-                    ),
-                  ],
+                      SizedBox(
+                        height: 20,
+                      ),
+                      ElevatedButton(
+                        child: Text('登录'),
+                        onPressed: () => submitForm(context),
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(200, 40),
+                          primary: Colors.white,
+                          onPrimary: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
-        mainAxisAlignment: MainAxisAlignment.center,
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
       ),
     );
   }
